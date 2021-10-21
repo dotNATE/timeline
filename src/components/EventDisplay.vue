362f72a0126ab53bbe07eb3eddbdaa86a13e5f3b
dotNate <somersetnathaniel@gmail.com>
2021-10-21 20:24:29 +0100

    added EventDisplay component and tests

--- src/components/EventDisplay.vue
+++ src/components/EventDisplay.vue
@@ -1,3 +1,19 @@
 <template>
-  <div></div>
+  <div class="event-card">
+    <h1 data-testid="title">{{ title }}</h1>
+    <div>
+      <span data-testid="year-release">{{ yearOfRelease }}</span>
+      <span data-testid="year-timeline">{{ yearInTimeline }}</span>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    yearOfRelease: String,
+    yearInTimeline: String,
+  },
+};
+</script>
--- tests/unit/EventDisplay.spec.js
+++ tests/unit/EventDisplay.spec.js
@@ -1,5 +1,44 @@
+import { mount } from "@vue/test-utils";
+import EventDisplay from "@/components/EventDisplay.vue";
+
 describe("EventDisplay", () => {
-  it("returns true", () => {
-    expect(true).toBe(true);
+  it("renders the event card", () => {
+    const wrapper = mount(EventDisplay);
+    const eventCardExists = wrapper.find(".event-card").exists();
+
+    expect(eventCardExists).toBe(true);
+  });
+
+  it("renders event name", () => {
+    const testText = "Test Title";
+    const wrapper = mount(EventDisplay, {
+      props: {
+        title: testText,
+      },
+    });
+    const titleText = wrapper.find('[data-testid="title"]').text();
+    expect(titleText).toEqual(testText);
+  });
+
+  it("renders year of release", () => {
+    const testText = "Event Test";
+    const wrapper = mount(EventDisplay, {
+      props: {
+        yearOfRelease: testText,
+      },
+    });
+    const releaseYearText = wrapper.find('[data-testid="year-release"]').text();
+    expect(releaseYearText).toEqual(testText);
+  });
+
+  it("renders year in timeline", () => {
+    const testText = "Event Test";
+    const wrapper = mount(EventDisplay, {
+      props: {
+        yearInTimeline: testText,
+      },
+    });
+    const timelineYear = wrapper.find('[data-testid="year-timeline"]').text();
+    expect(timelineYear).toEqual(testText);
   });
 });
